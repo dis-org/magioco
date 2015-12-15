6eab5ba04ba4b1752728642d2d5adff6bff91825
Shizen39 <mazzagiorgio96@gmail.com>
2015-12-15 09:19:55 +0100

    Amen

--- var.h
+++ var.h
@@ -1,15 +1,27 @@
 //variabili universali
 #include "struct.h"
+<<<<<<< HEAD
+char state;	
+char phase; //tipo state, ma per il combattimento
+=======
 char state;
+>>>>>>> 1bd6d5dd5c5a9b63228d2b0f6515f51b56fe58c5
 _Bool buffered;
 char id[128];
 short event_chosen;
 short enemy_chosen;
 short item_chosen;
 short use_chosen;
+<<<<<<< HEAD
+char name[64];
+short health;
+short defence;
+Choice_List Events;
+=======
 Choice_List Events;
 Choice_List Uses; //possibili utilizzi degli oggetti (predefinito)
 Choice_List Items; //?
 Choice_List Enemies; //?? non so se serve
+>>>>>>> 1bd6d5dd5c5a9b63228d2b0f6515f51b56fe58c5
 Item_List Bag;
 Enemy_List Battle;
