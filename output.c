c7f1dfa922d5bbcc2be2a31827422d67a1eece52
Shizen39 <mazzagiorgio96@gmail.com>
2015-12-16 19:35:32 +0100

    Amen x2

--- output.c
+++ output.c
@@ -5,29 +5,44 @@ extern Data_t Local;
 
 void print_Enemies()
 { 
-  for (int x=0; x<Local.Battle.enemies; x++)
+  for (int x=0; x<Battle.enemies; x++)
     {
-      printf("%c %s\nHp: ",x==Local.enemy_chosen? '[':' ', Local.Battle.First->name);
-      for (int y=0; y<Local.Battle.First->health; y++)
-        for(int z=0; z<Local.Battle.First->defence; z++)
+      printf("%c %s\nHp: ",x==enemy_chosen? '[':' ', Battle.First->name);
+      for (int y=0; y<Battle.First->health; y++)
+      	  printf("*");
+      for (int z=0; z<Battle.First->defence; z++)
           printf(")");
-      printf("%c\n", x==Local.enemy_chosen? ']':' ');
+      printf("%c\n", x==enemy_chosen? ']':' ');
     }
 }
-
+ 
 
 void print_Items()      //solo nome 
-{
-  
+{ 
+  Item_t* Tmp= Bag.First; 
+  for(int x= 1; x <= Bag.items; x++)
+    {
+      printf("%c %s\n", x==item_chosen? '>' : ' ',  Tmp->name);
+      Tmp= Tmp->Next;
+    }
 }
 
 void print_Events()
 {
-  printf("%s\n", Local.Events.text);
-  Choice_t* Temp= Local.Events.First; 
-  for(int x= 1; x <= Local.Events.choices; x++)
+  printf("%s\n", Events.text);
+  Choice_t* Temp= Events.First; 
+  for(int x= 1; x <= Events.choices; x++)
     {
-      printf("%c %s\n", x==Local.event_chosen? '>' : ' ', Temp->text);
+      printf("%c %s\n", x==event_chosen? '>' : ' ', Temp->text);
       Temp= Temp->Next;
     }
+}
+
+void print_Stat ()
+{
+	printf("%s\t", name);
+	for (int x=0; x<health; x++)
+        printf("*");
+    for(int y=0; y<defence; y++)
+        printf(")");
 }
