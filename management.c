8eb106424385871646e50adcaba93b06656b1ba6
Kappanneo <claudio_pannacci@libero.it>
2015-12-16 16:19:19 +0100

    aggiunto searchItem().

--- management.c
+++ management.c
@@ -1,7 +1,16 @@
-#include "var.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "management.h"
 
-
-void addAction(Enemy_t* Enemy, char* text, char type, short value);
-void addEnemy(char* name, short health, short defence);
-void addItem(Item_List* List, char* name, char type, int usev, unsigned short trwv, unsigned short defv, int uses);
+Item_t* searchItem(char* id, Item_List* List)
+{
+  Item_t* Ret= List->First;
+  while(Ret!=NULL)
+    {
+      if(!strcmp(Ret->name, id))
+	break;
+      Ret= Ret->Next;
+    }
+  return Ret;
+}
--- management.h
+++ management.h
@@ -1,4 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "var.h"
 
-
+void addAction(Enemy_t* Enemy, char* text, char type, short value);
+void addEnemy(char* name, short health, short defence);
+void addItem(Item_List* List, char* name, char type, int usev, unsigned short trwv, unsigned short defv, int uses);
+Item_t* searchItem(char* id, Item_List* List);
