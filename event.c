808f60931cb1de06fd40f532669dd4af8ae3d55a
Kappanneo <claudio_pannacci@libero.it>
2015-12-17 17:19:12 +0100

    aggiunte dichiazioni funzioni managemnt

--- event.c
+++ event.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+void addChoice(char* text, Choice_List* List)
+{
+  Choice_t* C= calloc(1,sizeof(Choice_t)); //controllo allocazione
+  if(!C)
+    {
+      fprintf(stderr,"Errore: allocazione non riuscita (addChoice)\n");
+      exit(EXIT_FAILURE);
+    }
+  strcpy(C->text, text);
+  if(!List->Last)
+    List->Last= List->First= C;
+  else
+    {
+      List->Last->Next= C;
+      List->Last= C;
+    }
+  List->choices++;
+}
+
 void select(char* text, short chosen, Choice_List* List)
 {
   Choice_t* p= List->First;
@@ -21,23 +40,4 @@ void deleteChoices(Choice_List* List)
     }
   List->Last=NULL;
   List->choices= 0;
-}
-
-void addChoice(char* text, Choice_List* List)
-{
-  Choice_t* C= calloc(1,sizeof(Choice_t)); //controllo allocazione
-  if(!C)
-    {
-      fprintf(stderr,"Errore: allocazione non riuscita (addChoice)\n");
-      exit(EXIT_FAILURE);
-    }
-  strcpy(C->text, text);
-  if(!List->Last)
-    List->Last= List->First= C;
-  else
-    {
-      List->Last->Next= C;
-      List->Last= C;
-    }
-  List->choices++;
 }
--- universal.h
+++ universal.h
@@ -5,19 +5,21 @@
 void battle(void);
 
 //event
+void addChoice(char* text, Choice_List* List); //aggiunge una scelta ad una Choice_List
 void select(char* text, short chosen, Choice_List* List); //ritorna il testo scelto
 void deleteChoices(Choice_List* List); //svuota una Choice_List
-void addChoice(char* text, Choice_List* List); //aggiunge una scelta ad una Choice_List
 
 //input
 void press_a(void); //ferma il programma fino a che non si preme 'a'
 _Bool choice(short* chosen, short choices); // rende 1 se viene premuto 'a' se effettua un cambiamento al valore chosed o allo stato rende 0
 
 //management
-void addAction(Enemy_t* Enemy, char* text, char type, short value);
-void addEnemy(char* name, short health, short defence);
 void addItem(Item_List* List, char* name, char type, int usev, unsigned short trwv, unsigned short defv, int uses);
 Item_t* searchItem(char* id, Item_List* List);
+void addEnemy(char* name, short health);
+void addAction(Enemy_t* Enemy, char* text, char type, short value);
+Enemy_t* searchEnemy(char* id, Enemy_List* List);
+void deleteEnemy(Enemy_t* Enemy);
 
 //output
 void next_page(); //divide graficamente le varie schermate
@@ -39,5 +41,6 @@ char* sstring(FILE*,char);
 void controle(FILE*,char,char);
 void readchoices(FILE*);
 void isearch();
+void esearch();
 void save(); // salva Date_t Local su file save.bin
 void load(); // carica i salvataggi (se esistono) e stampa relativo messaggio
