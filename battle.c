9884546de6eb37a5db8ccb37d77bc2731e4a93d1
Shizen39 <mazzagiorgio96@gmail.com>
2015-12-18 11:35:34 +0100

    Iniziando battle.c...

--- battle.c
+++ battle.c
@@ -3,7 +3,7 @@
 
 extern Data_t Local;
 
-void battle(void)
+char battle(void)
 {
   print_Enemies();
   print_Stats();
@@ -12,6 +12,49 @@ void battle(void)
     case'i':
       print_Items();
       if(choice(&Local.item_chosen, Local.Bag.items))
-        Local.phase= 'u';
+	  return Local.phase='u';
+	case 'u':
+	  print_Uses();
+	  if (choice(&Local.use_chosen, 5)) //percepisco che è sbagliata, 
+		switch(Local.use_chosen)
+        {
+          case 1: //usa su di te
+            me_Use ()
+            Local.health += Local.Bag.First->usevalue;
+            break;
+          case 2: //usa su nemico
+            enemy_Use();
+            
+            break;
+          case 3: //lancia
+            Trow();
+            
+            break;
+          case 4: //difenditi
+            Defend();
+            
+            break;
+          case 5: //inventario
+            break;  
+        }
+      Local.use_chosen==5 ? return Local.phase='i', return Local.phase='a';
+    case 'a':
+
+
+
     }
+}
+
+
+Enemy_List enemy_Use()
+{
+	
+}
+
+Enemy_List Trow()
+{
+}
+
+unsigned short Defend()
+{
 }
